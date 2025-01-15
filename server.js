112821cc8d7ae45bb4eb01ddd125be11132b5141
christian-ahanonu <ahanonuchristian116@gmail.com>
2025-01-15 01:42:02 +0100

    setup partials and server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
+
 /* ***********************
  * Require Statements
  *************************/
@@ -9,11 +11,51 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
+
+const livereload = require("livereload")
+const connectLiveReload = require("connect-livereload")
+
+ 
+
+/* ***********************
+ * LiveReload Server Setup
+ *************************/
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(__dirname + "/public");
+liveReloadServer.server.once("connection", () => {
+  setTimeout( () => {
+    liveReloadServer.refresh("/")
+  }, 100);
+});
+
+
+/* ***********************
+ * View Engine Templates
+ *************************/
+app.set("view engine", "ejs") 
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // path to find all layouts
+
+
+/* ***********************
+ * Middleware for LiveReload
+ *************************/
+app.use(connectLiveReload())
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+
+// Index route
+app.get("/", function(req, res) {
+  res.render('index', {title: "Home"})
+})
+
+
 
 /* ***********************
  * Local Server Information
@@ -21,6 +63,8 @@ app.use(static)
  *************************/
 const port = process.env.PORT
 const host = process.env.HOST
+
+
 
 /* ***********************
  * Log statement to confirm server operation
